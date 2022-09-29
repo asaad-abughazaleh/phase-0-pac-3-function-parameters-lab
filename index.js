bc19240a17d62ea219483a407519bb13c16aa4c3
Asaad AbuGhazaleh <asaad.ag.98@gmail.com>
2022-09-29 23:36:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+
+function introductionWithLanguage(value1, value2) {
+    return `Hi, my name is ${value1} and I am learning to program in ${value2}.`;
+}
+
+function sayHelloTo(Asaad = "User") {
+    console.log(`Hello, ${firstName}!`);
+}
+
+function introductionWithLanguageOptional(name, language = 'JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
